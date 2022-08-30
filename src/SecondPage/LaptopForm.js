877025717d440c683a35611d434f3a6362abfab2
misho <mishiko8gb@gmail.com>
2022-08-31 01:31:01 +0400

    update some UI

--- src/SecondPage/LaptopForm.js
+++ src/SecondPage/LaptopForm.js
@@ -1,9 +1,12 @@
 import React from "react";
 import Button from "../UI/Button";
+import Input from "../UI/Input";
 
 import classes from "../SecondPage/LaptopForm.module.css";
 
 const LaptopForm = () => {
+  const symbols = "${!@#$%^&*()_+=";
+
   return (
     <form className={classes.mainForm}>
       <label htmlFor="file-upload" className={classes.uploadPhoto}>
@@ -11,6 +14,39 @@ const LaptopForm = () => {
         <p>Drag your files here or click in this area.</p>
         <Button type="submit">ატვირთე</Button>
       </label>
+
+      <div className={classes.nameBrand}>
+        <label htmlFor="name" className={classes.mainLaptName}>
+          <span>ლეპტოპის სახელი</span>
+          <Input type="text" placeholder="HP" className={classes.laptopName} />
+          <text>ლათინური ასოები, ციფრები,{symbols} </text>
+        </label>
+
+        <select className={classes.selectBrand}>
+          <option value="0">Select car:</option>
+          <option value="1">Audi</option>
+        </select>
+      </div>
+
+      <div className={classes.line1}></div>
+
+      <div className={classes.mainCPU}>
+        <select className={classes.selectCPU}>
+          <option value="0">CPU</option>
+          <option value="1">intel</option>
+        </select>
+
+        <label htmlFor="name" className={classes.core}>
+          <span>CPU-ს ბირთვი</span>
+          <Input type="text" placeholder="14" className={classes.coreInput} />
+          <text>მხოლოდ ციფრები</text>
+        </label>
+        <label htmlFor="name" className={classes.core}>
+          <span>CPU-ს ნაკადი</span>
+          <Input type="text" placeholder="365" className={classes.coreInput} />
+          <text>მხოლოდ ციფრები</text>
+        </label>
+      </div>
     </form>
   );
 };
